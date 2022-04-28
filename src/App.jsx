844e521a971051558b4c5247d23b35b05ce49911
m-ghiringhelli <mghiring@gmail.com>
2022-04-28 14:50:39 -0700

    initial style of quotecard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
+import Quote from "./components/Quote";
 import { getQuotes } from "./services/quotes";
+import style from './App.css';
 
 
 export default function App() {
@@ -9,6 +11,17 @@ export default function App() {
     }
     getData();
   }, []);
+  
+  const quote = {
+    quote: "Shut up, brain. I got friends now. I don't need you anymore.",
+    character: "Lisa Simpson",
+    image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
+    characterDirection: "Right"
+  }
 
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Quote quote={quote}/>
+    </>
+  );
 }
